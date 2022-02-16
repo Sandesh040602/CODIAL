5707ca835141e564a815bfb020750ee8c4a4f2fb
sandesh <sandeshchand046@gmail.com>
2022-02-16 12:52:17 +0530

    long break;

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/posts_controllers');
+const postController = require('../controllers/posts_controllers');
 
-router.get('/profile',userController.posts);
+router.get('/profile',postController.posts);
 
 module.exports = router;
