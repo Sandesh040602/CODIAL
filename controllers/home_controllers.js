392a31fe12d3820eeef0a8d06603fcbc4a004fd9
sandesh <sandeshchand046@gmail.com>
2022-02-18 00:56:00 +0530

    data is displaying in home page

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,6 +1,17 @@
+const express = require('express');//requiring express
+//connecting to the database new_clist
+const db = require('../config/mongoose');
+console.log('post hi');
+const Todo = require('../models/todo');
+
+
 module.exports.home = function(req,res){
-   return res.render('home',{
-       title: "home"
+   Todo.find({},function(err,todo){
+       if(err){console.log('their is an error:',err);}
+       return res.render('home',{
+           title:'hitlist',
+           todo_lis: todo
+       })
    })
 }
 module.exports.secondhome = function(req,res){
